eb60d61e347a11727df62a497751a47952d62fb2
Nicolás Serrano Pérez <nicolasserrano225417@correo.itm.edu.co>
2021-08-22 19:10:52 -0500

    add entity

---
+++ todonico.Functions/Entities/todoEntities.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todonico.Functions.Entities
+{
+    public class todoEntities : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
