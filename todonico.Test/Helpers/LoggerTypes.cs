c902ec0914a84c4958309d52ea159b3dc5923a96
Nicolás Serrano Pérez <nicolasserrano225417@correo.itm.edu.co>
2021-08-22 22:17:19 -0500

    pruebas unitarias

---
+++ todonico.Test/Helpers/LoggerTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todonico.Test.Helpers
+{
+    public enum LoggerTypes
+    {
+        Null,
+        List
+    }
+}
---
+++ todonico.Test/Helpers/NullScope.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace todonico.Test.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; } = new NullScope();
+        public void Dispose() { }
+        private NullScope() { }
+    }
+}
